61ba0aac8f8e915339ea31135317319930f955e6
yuma-kato-cosmosweb-com <103395578+yuma-kato-cosmosweb-com@users.noreply.github.com>
2022-05-30 11:28:42 +0900

    初版

---
+++ FileCapaApp/FileCapaApp/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace FileCapaApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FilePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Capacity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.csv保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(380, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "右の参照ボタンをクリックしてフォルダを選択してください";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(410, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "参照";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "検索フォルダ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(513, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "容量(KB)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(516, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(93, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(628, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "以上";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(695, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 57);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "検索";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.No,
+            this.FilePath,
+            this.FileName,
+            this.Capacity});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 161);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(805, 450);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // No
+            // 
+            this.No.HeaderText = "No";
+            this.No.Name = "No";
+            this.No.Width = 40;
+            // 
+            // FilePath
+            // 
+            this.FilePath.FillWeight = 350F;
+            this.FilePath.HeaderText = "ファイルパス";
+            this.FilePath.Name = "FilePath";
+            this.FilePath.Width = 350;
+            // 
+            // FileName
+            // 
+            this.FileName.FillWeight = 180F;
+            this.FileName.HeaderText = "ファイル名";
+            this.FileName.Name = "FileName";
+            this.FileName.Width = 180;
+            // 
+            // Capacity
+            // 
+            this.Capacity.HeaderText = "容量";
+            this.Capacity.Name = "Capacity";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(848, 28);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.csv保存ToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(63, 24);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // csv保存ToolStripMenuItem
+            // 
+            this.csv保存ToolStripMenuItem.Name = "csv保存ToolStripMenuItem";
+            this.csv保存ToolStripMenuItem.Size = new System.Drawing.Size(134, 26);
+            this.csv保存ToolStripMenuItem.Text = "csv保存";
+            this.csv保存ToolStripMenuItem.Click += new System.EventHandler(this.csv保存ToolStripMenuItem_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "ファイル一覧";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(695, 133);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 22);
+            this.textBox3.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(692, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "見つかった件数";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 622);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "容量検索";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem csv保存ToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FilePath;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Capacity;
+    }
+}
+
---
+++ FileCapaApp/FileCapaApp/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileCapaApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Err(int type)      //  エラーメッセージ
+        {
+            string Message = "";
+            switch (type)
+            {
+                case 1:
+                    Message = "参照ボタンをクリックしフォルダを選んでください。";
+                    break;
+                case 2:
+                    Message = "容量の欄に正の整数を入力してください。";
+                    break;
+                default:
+                    break;
+            }
+            MessageBox.Show(Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button1_Click(object sender, EventArgs e)          //  参照ボタンクリック
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();        //FolderBrowserDialogクラスのインスタンスを作成
+            fbd.Description = "フォルダを指定してください。";
+            //ルートフォルダを指定する
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;            //デフォルトでDesktop
+            //ユーザーが新しいフォルダを作成できるようにする
+            fbd.ShowNewFolderButton = true;                          //デフォルトでTrue
+
+            if (fbd.ShowDialog(this) == DialogResult.OK)            //ダイアログを表示する
+            {
+                textBox1.Text = fbd.SelectedPath;                //選択されたフォルダを表示する
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)      //  検索ボタンクリック
+        {
+            dataGridView1.Rows.Clear();             //  表初期化
+            string Path = "";
+            int Capacity = 0;
+            int count = 0;
+            int count2 = 0;
+            var diPath = new List<string>();
+            Path = textBox1.Text;
+
+            if (Path == "")             //  空白ではないか確認
+            {
+                Err(1);
+            }
+
+            else
+            {
+                try
+                {
+                    Capacity = int.Parse(textBox2.Text);        //  int型に変換できるか確認
+                }
+                catch
+                {
+                    Err(2);
+                }
+
+                try
+                {
+                    DirectoryInfo DirInfo1 = new DirectoryInfo(Path);
+                    count = FileCapa(DirInfo1, Capacity, count);
+                    DirectoryInfo DirInfo2 = DirInfo1;
+
+                    while (true)
+                    {
+                        try
+                        {
+                            foreach (DirectoryInfo di in DirInfo2.GetDirectories())//フォルダ内のフォルダを取得
+                            {
+                                diPath.Add(di.FullName);
+                                DirectoryInfo DirInfo3 = new DirectoryInfo(di.FullName);
+                                count = FileCapa(DirInfo3, Capacity, count);
+                            }
+                            DirInfo2 = new DirectoryInfo(diPath[count2]);   //  新たなフォルダのパス
+                            count2++;
+                        }
+                        catch
+                        {
+                            break;
+                        }
+                    }
+                    textBox3.Text = count.ToString();           //  何件あったか表示
+                }
+                catch
+                {
+                    Err(1);
+                }
+            }
+        }
+
+        private void csv保存ToolStripMenuItem_Click(object sender, EventArgs e)    //  csv保存ボタンクリック
+        {
+            SaveFileDialog sa = new SaveFileDialog();            //SaveFileDialogを生成する
+            sa.Title = "ファイルを保存する";
+            sa.InitialDirectory = @"C:\";
+            sa.FileName = @"検索結果.csv";
+            sa.FilterIndex = 1;
+
+            DialogResult result = sa.ShowDialog();            //オープンファイルダイアログを表示する
+
+            if (result == DialogResult.OK)          //「保存」ボタンが押された時の処理
+            {
+                string FilePath = sa.FileName;       // 指定されたファイルのパスが取得
+
+                StreamWriter sw = new StreamWriter(FilePath, false, Encoding.GetEncoding("SHIFT-JIS"));     // CSVファイルオープン
+                for (int r = 0; r <= dataGridView1.Rows.Count - 1; r++)
+                {
+                    for (int c = 0; c <= dataGridView1.Columns.Count - 1; c++)
+                    {
+                        string hedder = "";
+                        string dt = "";
+                        if ( (r == 0) && (c == 0) )
+                        {
+                            for (int h = 0; h <= dataGridView1.Columns.Count - 1; h++)
+                            {
+                                hedder = dataGridView1.Columns[h].HeaderCell.Value.ToString() + ",";
+                                sw.Write(hedder);
+                            }
+                            sw.Write("\n");
+                        }
+                        if (dataGridView1.Rows[r].Cells[c].Value != null)                // DataGridViewのセルのデータ取得
+                        {
+                            dt = dataGridView1.Rows[r].Cells[c].Value.ToString() + ",";
+                        }
+                        sw.Write(dt);                        // CSVファイル書込
+                    }
+                    sw.Write("\n");
+                }
+                sw.Close();                // CSVファイルクローズ
+            }
+            else if (result == DialogResult.Cancel)      //「キャンセル」ボタンまたは「×」ボタンが選択された時の処理
+            {
+            }
+        }
+        private int FileCapa(DirectoryInfo Dir, int Capa, int count)
+        {
+            foreach (FileInfo fi in Dir.GetFiles())//フォルダ内の全ファイルを取得
+            {
+                if (fi.Length / 1024 > Capa)        // 設定した値との比較
+                {
+                    var addValues = new string[]
+                    {
+                            (count + 1).ToString(), fi.DirectoryName, fi.Name, (fi.Length / 1024).ToString() + "KB"
+                    };
+                    dataGridView1.Rows.Insert(count, addValues);        //  表に出力
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
